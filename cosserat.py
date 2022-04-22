5b0f4b405bca66c2fe8625953e90788bef98a3d3
Ravinda Gunaratne <ravindag@hotmail.com>
2022-04-22 23:40:47 +0100

    logm added

--- cosserat.py
+++ cosserat.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class CosseratRod:
 
-    def __init__(self, segments=10, seg_length=10, position=[[0],[0],[0]]):
+    def __init__(self, segments=10, seg_length=10.0, position=[[0.0],[0.0],[0.0]]):
         #Basic geometric properties
         self.N = segments
         self.l_0 = seg_length
@@ -21,15 +21,15 @@ class CosseratRod:
 
         #Mass, inertia, rigidities
         self.m = (seg_length * self.A * self.rho) * np.ones((1,self.N+1))
-        self.m[0,0] /= 2
-        self.m[0,self.N] /= 2
-        self.I = ((self.m[0,1] * self.A ** 2) / (4 * np.pi)) * np.diag(np.array([1,1,2]))
-        self.B = np.diag(np.array([39000, 39000, 13000]))
-        self.S = np.diag(np.array([16000, 16000, 34000]))
+        self.m[0,0] *= 0.5
+        self.m[0,self.N] *= 0.5
+        self.I = ((self.m[0,1] * self.A ** 2) / (4 * np.pi)) * np.diag(np.array([1.0,1.0,2.0]))
+        self.B = np.diag(np.array([39000.0, 39000.0, 13000.0]))
+        self.S = np.diag(np.array([16000.0, 16000.0, 34000.0]))
         
     def expm(self,u):
         theta = np.linalg.norm(u)
-        if theta > 0:
+        if theta > 0.0:
             u /= theta
             U = np.array([[0,-u[2],u[1]],[u[2],0,-u[0]],[-u[1],u[0],0]])
             return np.identity(3) + np.sin(theta) * U + np.cos(theta) * U * U
@@ -37,8 +37,15 @@ class CosseratRod:
         #Change this to act on a 3xN array of u values
     
     def logm(self,R):
+        theta = np.arccos(0.5 * (np.einsum('ii',R)-1.0))
+        skew = R - np.einsum('ij->ji',R)
+        skew = np.array([[skew[1,2]],[skew[0,2]],[-skew[0,1]]]) 
+        if abs(theta) > 1e-7:
+            sin_term = 0.5 + (1.0/12.0) * theta ** 2 + (7.0/720.0) * theta ** 4 * (31.0/30240.0) * theta ** 6
+        else:
+            sin_term = theta / (0.5 * np.sin(theta))
+        return sin_term * skew
         #write this to act on 3x3N array of orientation bases
-        pass
     
     def update_kappa(self):
         pass
@@ -74,5 +81,6 @@ print(filament.B)
 print(filament.x[:,3])
 print(filament.expm(filament.x[:,0]))
 filament.update_Q(0.001)
+filament.logm(filament.B)
 
 
