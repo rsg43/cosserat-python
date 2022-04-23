32ec1da9420fc12994263a5d967157810c1522e6
Ravinda Gunaratne <ravindag@hotmail.com>
2022-04-23 15:16:14 +0100

    small changes for simulation to be stable

--- cosserat-cantilever.py
+++ cosserat-cantilever.py
@@ -6,9 +6,9 @@ np.set_printoptions(precision=3)
 filament = CosseratRod()
 
 ext_F=np.zeros((3,filament.N+1))
-ext_F[0,filament.N] = 0.1
+ext_F[0,filament.N] = 1
 
-filament.symplectic(timespan=0.5,method='VV',conditions=['clamp_0'],ext_F=ext_F)
+filament.symplectic(timespan=10,method='VV',conditions=['clamp_0'],ext_F=ext_F)
 print(filament.x)
-print(filament.v)
-print(filament.w)
+# print(filament.v)
+# print(filament.w)
