312ea1e0037bb24899658e510d94faa37b076ee3
Ravinda Gunaratne <ravindag@hotmail.com>
2022-04-24 00:04:02 +0100

    finished initial commenting of code

--- cosserat.py
+++ cosserat.py
@@ -137,31 +137,39 @@ class CosseratRod:
         #update sigma and kappa
         self.update_sigma()
         self.update_kappa()
-        #calculate internal strain
+        #calculate shear/stretch strain
         n_temp = np.einsum('ij,j...->j...', self.S, self.sigma)
         #take strain to laboratory frame
         Qn_temp = np.einsum('jik,jk->ik',self.Q,n_temp) / self.e 
         #update dvdt
         dvdt = (self.diff(Qn_temp) + ext_F) / self.m
-
+        #calculate bend/twist strain
         tau_temp = np.einsum('ij,j...->i...', self.B, self.kappa) / np.power(self.ee,3)
         kappa_temp = np.cross(self.kappa, tau_temp, axisa=0, axisb=0, axisc=0) * self.D_0
+        #calculate torsional effect of shear
         shear_temp = np.einsum('ijk,jk->ik', self.Q, self.l / self.l_norm)
         shear_temp = np.cross(shear_temp, n_temp, axisa=0, axisb=0, axisc=0) * self.l_0
+        #temporarily define quantity
         dilatation_temp = np.einsum('ij,j...->i...',self.I,self.w) / self.e
+        #calculate rigid body term (Euler)
         rigid_temp = np.cross(dilatation_temp, self.w, axisa=0, axisb=0, axisc=0)
+        #calculate dilatation term
         dilatation_temp *= self.e_v / self.e
+        #update dwdt
         dwdt = self.diff(tau_temp) + self.quad(kappa_temp) + shear_temp + dilatation_temp + rigid_temp + ext_C
         dwdt *= np.tile(self.e,(3,1)) / np.einsum('ij->ji',np.tile(np.diag(self.I),(10,1)))
-
+        #return accelerations
         return dvdt, dwdt
 
     def update_conditions(self,conditions):
+        #no conditions set
         if conditions == []:
             return
+        #clamped at 0 end
         if 'clamp_0' in conditions:
             self.v[:,0:2] = np.zeros((3,2))
             self.w[:,0] = np.zeros((3,))
+        #clamped at N end
         if 'clamp_N' in conditions:
             self.v[:,self.N-1:self.N+1] = np.zeros((3,2))
             self.w[:,self.N] = np.zeros((3,))
@@ -173,32 +181,43 @@ class CosseratRod:
         if isinstance(ext_C,np.ndarray) == False:
             ext_C = np.zeros((3,self.N))
 
-        #Set integrating method    
+        #Set integrating method as position extended Forrest-Ruth like   
         if method == 'PEFRL':
             xi = 0.1786178958448091
             lmbda = -0.2123418310626054
             chi = -0.06626458266981849
             a = np.array([xi,chi,1-2*(xi+chi),chi,xi])
             b = np.array([0.5*(1-2*lmbda),lmbda,lmbda,0.5*(1-2*lmbda)])
+        #Set integrating method as velocity Verlet
         elif method == 'VV':
             a = np.array([0.5,0.5])
             b = np.array([1])
+        #Set integrating method as position Verlet
         elif method == 'PV':
             a = np.array([0,1])
             b = np.array([0.5,0.5])
+        #Raise error if method not recognised
         else:
             raise ValueError('Incompatible integrator specified')
 
-        #Run simulation loop
+        #Set number of steps in simulation
         numsteps = int(timespan / dt)
+        #Run simulation loop
         for ii in range(numsteps):
+            #Loop through integrator coefficents
             for jj in range(max(len(a),len(b))):
+                #check to see if there is a first velocity step
                 if a[jj] != 0:
+                    #update accelerations
                     dvdt, dwdt = self.update_acceleration(ext_F,ext_C)
+                    #update velocities
                     self.update_v(dvdt, a[jj] * dt, dissipation)
                     self.update_w(dwdt, a[jj] * dt, dissipation)
+                    #enforce boundary conditions
                     self.update_conditions(conditions)
+                #check to see if there is a final position step
                 if jj < len(b):
+                    #update positions and orientations
                     self.update_x(b[jj] * dt)
                     self.update_Q(b[jj] * dt)
 
