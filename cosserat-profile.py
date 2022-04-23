307580b825e68b4d7531916d2afae4921bd76a86
Ravinda Gunaratne <ravindag@hotmail.com>
2022-04-23 12:38:13 +0100

    logm_test added

--- cosserat-profile.py
+++ cosserat-profile.py
@@ -2,4 +2,11 @@ from cosserat import CosseratRod
 import cProfile
 
 filament = CosseratRod()
-cProfile.run('filament.symplectic()')
+cProfile.run('filament.symplectic(timespan=100)')
+
+def logm_test():
+	for i in range(1000000):
+		filament.logm(filament.Q[:,:,3])
+
+
+cProfile.run('logm_test()')
