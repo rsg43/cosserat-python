27f5ca84cf342e7253b876872257ddfb04d339a8
Ravinda Gunaratne <ravindag@hotmail.com>
2022-04-23 14:00:57 +0100

    cantilever test updated

--- cosserat-cantilever.py
+++ cosserat-cantilever.py
@@ -1,5 +1,11 @@
 from cosserat import CosseratRod
+import numpy as np
 
 filament = CosseratRod()
-filament.symplectic(timespan=10,conditions=['clamp_0'])
+ext_F=np.zeros((3,filament.N+1))
+ext_F[0,filament.N] = 0.1
+print(ext_F)
+filament.symplectic(timespan=0.5,conditions=['clamp_0'],ext_F=ext_F)
 print(filament.x)
+print(filament.v)
+print(filament.w)
