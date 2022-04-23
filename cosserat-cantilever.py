5bdcaf84304539aa9c1836df3c7152f21f4927a2
Ravinda Gunaratne <ravindag@hotmail.com>
2022-04-23 14:47:09 +0100

    updated printing precision

--- cosserat-cantilever.py
+++ cosserat-cantilever.py
@@ -1,11 +1,14 @@
 from cosserat import CosseratRod
 import numpy as np
 
+np.set_printoptions(precision=3)
+
 filament = CosseratRod()
+
 ext_F=np.zeros((3,filament.N+1))
 ext_F[0,filament.N] = 0.1
-print(ext_F)
-filament.symplectic(timespan=0.5,conditions=['clamp_0'],ext_F=ext_F)
+
+filament.symplectic(timespan=0.5,method='VV',conditions=['clamp_0'],ext_F=ext_F)
 print(filament.x)
 print(filament.v)
 print(filament.w)
