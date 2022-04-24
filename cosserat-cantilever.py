1a84bc9d58580dd97cb9258ad9377118fc5f59e8
Ravinda Gunaratne <ravindag@hotmail.com>
2022-04-24 01:46:32 +0100

    reverted to default inputs for cantilever

--- cosserat-cantilever.py
+++ cosserat-cantilever.py
@@ -8,7 +8,7 @@ filament = CosseratRod()
 ext_F=np.zeros((3,filament.N+1))
 ext_F[0,filament.N] = 2
 
-filament.symplectic(timespan=100,dt=0.01,method='PV',conditions=['clamp_0'],ext_F=ext_F,dissipation=1)
+filament.symplectic(timespan=10,conditions=['clamp_0'],ext_F=ext_F,dissipation=1)
 
 print(filament.x)
 print('====x=====')
