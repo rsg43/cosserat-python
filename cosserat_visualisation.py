fdfeb41ecbcad58f9d9ba60a0cf8d9dfad002f84
Ravinda Gunaratne <ravindag@hotmail.com>
2022-04-24 14:43:56 +0100

    started work on 3d cylinder plotter

--- cosserat_visualisation.py
+++ cosserat_visualisation.py
@@ -13,4 +13,18 @@ class CosseratVisualisation:
 		ax.plot3D(x,y,z)
 		plt.show()
 
+	def cylinder_coords(self,radius,length,orientation):
+		# z = np.linspace(0, height_z, 50)
+	 #    theta = np.linspace(0, 2*np.pi, 50)
+	 #    theta_grid, z_grid=np.meshgrid(theta, z)
+	 #    x_grid = radius*np.cos(theta_grid) + center_x
+	 #    y_grid = radius*np.sin(theta_grid) + center_y
+	 #    return x_grid,y_grid,z_grid
+		pass
+
+	def cylinder_plot(self):
+		pass
+
+
+
 
