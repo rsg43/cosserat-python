aae8ba7d20eeee72a22715cdd4377a0c131b4fe3
Ravinda Gunaratne <ravindag@hotmail.com>
2022-04-26 23:59:02 +0100

    simulation file for linked filaments added

---
+++ cosserat-linked-filaments.py
@@ -0,0 +1,14 @@
+from cosserat import CosseratRod
+from cosserat_multiple_filaments import CosseratMultipleFilament
+from cosserat_visualisation import CosseratVisualisation
+
+
+num_fils = 3
+filaments = [None] * num_fils
+for ii in range(num_fils):
+	filaments[ii] = CosseratRod()
+links = [[[0,0],[1,3]],[[1,7],[2,5]],[[2,3],[0,4]],[[1,9],[0,8]],[[2,7],[0,5]]]
+
+
+filament_store = CosseratMultipleFilament(filaments)
+filament_store.symplectic(links=links)
